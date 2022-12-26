f6e1fbbbc943bed4b08aa6606ede339b6e3e75fa
Emilija Cojbasic <ecojbasic@elfak.rs>
2022-12-26 16:55:53 +0100

    Implementacija ChatHub-a

---
+++ Anonymer/Anonymer/Hub/ChatHub.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using ServiceStack.Redis;
+using Models;
+using System.Text.Json;
+using ServiceStack.Logging;
+using ServiceStack.Messaging;
+using System.Xml.Linq;
+
+namespace Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly static Dictionary<string,string> connections = new Dictionary<string, string>();
+
+        readonly RedisClient redis = new("redis://default:redispw@localhost:49157");
+
+        IRedisSubscription subscription;
+        public ChatHub()
+        {
+            subscription = redis.CreateSubscription();
+            subscription.OnMessage = (channel, msg) =>
+            {
+                var chatMessage = JsonSerializer.Deserialize<ChatMessage>(msg);
+                Clients.Client(connections[chatMessage.RecipientID]).SendAsync("RecieveMessage", chatMessage);
+            };
+        }
+
+        // Send a message
+        public async Task SendMessage(ChatMessage message)
+        {
+            // Serialize message data
+            var messageData = JsonSerializer.Serialize(message);
+
+            // Publish message to Redis channel
+            redis.PublishMessage("chat", messageData);
+        }
+
+        public void Subscribe(string userID)
+        {
+            connections.Add(userID, Context.ConnectionId);
+        }
+
+        public void Unsubscribe(string userID)
+        {
+            connections.Remove(userID);
+        }
+
+    }
+}
--- Anonymer/Anonymer/Program.cs
+++ Anonymer/Anonymer/Program.cs
@@ -1,3 +1,5 @@
+using Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -24,8 +26,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
+app.MapHub<ChatHub>("/chathub");
 app.UseCors("CORS");
 
 // Configure the HTTP request pipeline.
